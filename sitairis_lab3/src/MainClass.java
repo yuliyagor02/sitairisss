7dfdffa1afa9fe772ebafb4923889f86e436a125
Gorodnaya Yuliya <gorodnayayuliya02@gmail.com>
2022-10-24 10:13:42 +0300

    second commit

--- sitairis_lab3/src/MainClass.java
+++ sitairis_lab3/src/MainClass.java
@@ -2,5 +2,6 @@ public class MainClass {
     public static void main(String[] args){
         LanguageCourse obj = new LanguageCourse();
         System.out.println("Index of the course is "+obj.getCourseIndex());
+        System.out.println("Name of the course is "+obj.getCourseName());
     }
 }
